698bc8d2643c84d594de4682fc8d9447d17af952
andres06-hub <andressierrarojas7@gmail.com>
2023-02-20 02:11:23 -0500

    CREATED: header

---
+++ src/Components/Layout/Header/Header.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import logo from '../../../Assets/img/Logo.svg';
+import as from '../../../Assets/img/as.svg';
+
+import './Header.css';
+
+export const Header = () => {
+  return (
+    <header className='header-container'>
+      <nav>
+        <section className='nav-content'>
+          <div className='logo'>
+            <img src={as} alt="logo AS" />
+          </div>
+          <div>
+            <ul className='links'>
+              <li>
+                <a href="https://www.linkedin.com/in/andressierrarojas/" target="_blank">
+                  <FontAwesomeIcon className='networks linkedin' icon={['fab', 'linkedin-in']}/>
+                </a>
+                <span className='icon'><i></i></span>
+                {/* <span className='title'>Linkedin</span> */}
+              </li>
+              <li>
+                <a href="https://www.github.com/andres06-hub/" target="_blank">
+                  <FontAwesomeIcon className='networks github' icon={['fab', 'github']}/>
+                </a>
+                <span className='icon'><i></i></span>
+                {/* <span className='title'>GitHub</span> */}
+              </li>
+              <li>
+                <a href="https://twitter.com/Andressr06" target="_blank">
+                  <FontAwesomeIcon className='networks twitter' icon={['fab', 'twitter']}/>
+                </a>
+                <span className='icon'><i></i></span>
+                {/* <span className='title'>Twitter</span> */}
+              </li>
+            </ul>
+          </div>
+        </section>
+      </nav>
+      <article className='descrip-container'>
+        <div className='descrip-content'>
+          <h2>Andres Felipe Sierra Rojas</h2>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi quos corrupti non necessitatibus blanditiis harum quidem nesciunt quibusdam suscipit iste id aperiam illo veniam magni quaerat, ea cupiditate repudiandae repellendus.</p>
+        </div>
+      </article>
+      <section className='me-container'>
+        <div className='img-container'>
+          <img src={logo} alt="img AS" />
+        </div>
+      </section>
+    </header>
+  )
+}
