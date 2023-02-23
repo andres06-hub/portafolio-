2f1701ff5b32a6248182edfe102ba29bcf0c2f38
andres06-hub <andressierrarojas7@gmail.com>
2023-02-22 21:39:40 -0500

    ADD Content page

--- src/Modules/Home/Home.tsx
+++ src/Modules/Home/Home.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Header } from '../../Components/Layout/Header/Header'
+import { Content } from '../../Components/Layout/Content/Content'
+import { RouterLink } from '../../Core/Router/Link';
 
 export const Home = () => {
   return (
     <>
       <Header/>
+      {/* <RouterLink to='/no'>ir notfound</RouterLink> */}
+      <Content/>
     </>
   )
 }
