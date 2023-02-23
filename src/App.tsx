b87bbe4f1fa91a2172a244018b1ddb23c51aa479
andres06-hub <andressierrarojas7@gmail.com>
2023-02-22 21:36:37 -0500

    ADD: Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Home } from './Modules/Home/Home';
-import { NotFound } from './Modules/not-found/NotFound';
+import React, { Suspense, lazy } from 'react';
+
+import { Router } from './Core/Router/Router';
+import { Route } from './Core/Router/Route';
+
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
+import { IRoutes } from './Shared/interfaces/IRoutes';
 
 library.add(fab, fas);
 
+//Application lazyLoading
+const lazyHomePage: React.LazyExoticComponent<() => JSX.Element> = lazy(() =>
+  import('./Modules/Home/Home')
+    .then(({ Home }) => ({ default: Home })),
+);
+const lazyNotFoundPage: React.LazyExoticComponent<() => JSX.Element> = lazy(() => 
+  import('./Modules/NotFound/NotFound')
+    .then(({ NotFound }) => ({ default: NotFound })), 
+);
+const lazySearchPage: React.LazyExoticComponent<({ routeParams }: any) => JSX.Element> = lazy(() => 
+  import('./Modules/Search/Search')
+    .then(({ Search }) => ({ default: Search })), 
+);
+
+const appRoutes: IRoutes[] = [
+  {
+    path: '/search/:query',
+    Component: lazySearchPage,
+  }
+]
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}>
-          <Route index element={<Home />} />
-          {/* <Route path="blogs" element={<Blogs />} />
-          <Route path="contact" element={<Contact />} /> */}
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <main>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Router routes={appRoutes} defaultComponent={ lazyNotFoundPage }>
+          <Route path='/' Component={ lazyHomePage }/>
+          <Route path='/home' Component={ lazyHomePage }/>
+        </Router>
+      </Suspense>
+    </main>
   );
+  
 }
 
 export default App;
