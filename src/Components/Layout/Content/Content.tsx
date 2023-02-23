bec05478ea5e71fbd521e8b42658b8ea82ce07d9
andres06-hub <andressierrarojas7@gmail.com>
2023-02-22 21:38:36 -0500

    CREATED: content about me

---
+++ src/Components/Layout/Content/Content.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Content.css';
+
+export const Content = () => {
+  return (
+    <>
+      <div className='first-container'></div>
+      <div className='c-descrip-container'>
+        <article className='c-content'>
+          <h2>Andres Felipe Sierra Rojas</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt ut quibusdam nostrum labore repudiandae facere quaerat eum eius eos cum. Repellat reprehenderit maiores esse, laborum fugiat error quaerat distinctio asperiores!</p>
+        </article>
+      </div>
+      <section className='content-container'>
+        <div className='c-img-container'>
+          
+        </div>
+        <div className='c-container'>
+
+        </div>
+      </section>
+    </>
+  )
+}
